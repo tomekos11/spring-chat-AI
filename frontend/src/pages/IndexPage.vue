25b99245fc906c1367e6ea700bb7d43dbe13a4ea
Tomasz Slapinski <tom.slapinski@gmail.com>
2024-06-05 16:58:49 +0200

    init

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    {{ xd }}
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const xd = ref('xD');
+</script>
